bc125dd518d0a19e0d17c40cf5a6ee7b655df213
Billy Fryer <billyfryer@att.net>
2022-04-20 21:50:35 -0400

    Skeleton!

---
+++ Parsing Files/302 Short Track Parsing.R
@@ -0,0 +1,79 @@
+# Short Track Parsing
+
+# Libraries
+library(tidyverse)
+library(jsonlite)
+library(lubridate)
+
+# Read in All File Names
+# Code Stolen From:
+# https://www.geeksforgeeks.org/read-all-files-in-directory-using-r/#:~:text=To%20list%20all%20files%20in,files%20in%20the%20specified%20directories.
+all_files <- list.files(path = "Output Folder/302 Short Track JSONs",
+                        # To make sure I grab only the relevant files 
+                        pattern = "Match ID")
+
+for (json_file_name in all_files){
+  
+  json_file_name <- all_files[1]
+  # Sanity Check
+  print(json_file_name)
+  
+  # Read in the json file
+  # Don't know how this works, but it does.
+  # Stolen From Stack Overflow:
+  # https://stackoverflow.com/questions/38074926/unable-to-parse-locally-stored-json-file-with-special-character-like-backslash
+  file_path <- paste0("Output Folder/302 Short Track JSONs/", json_file_name)
+  raw_json <- fromJSON(gsub("\\\\","",readLines(file_path)))
+  
+  # Date of Match and Gender
+  Date <- raw_json$Result$PhaseList$DateTimes$Start$c_Local
+  # Gender is in the json file name
+  Event <- str_split(json_file_name, 
+                     pattern = "Match ID", 
+                     n = 2)[[1]][1] %>% 
+    # Trim Whitespace
+    str_trim()
+  
+  Results <- raw_json$Result$PhaseList$ParticipantList
+  # Sports that have a qualifying and finals need the do.call
+  Results <- do.call("bind_rows", Results)
+  
+  # Always Unlist Phase Result List b/c nothing there
+  Results$PhaseResultList <- unlist(Results$PhaseResultList)
+  
+  
+  # Two Different Types: Team and Individual
+  if(str_detect(json_file_name, pattern = "Team", negate = TRUE)) {
+    # Individual Events Don't have a Team Member List
+    Results$TeamMemberList <- unlist(Results$TeamMemberList)
+    Full_Results <- Results
+  } else {
+    # Make a Key Variable For joining on Team members
+    Results <- Results %>% mutate(key = 1:nrow(Results))
+    
+    # Results looks ok, just have to figure out the team members now 
+    TeamMembers <- Results$TeamMemberList
+    TeamMembers <- lapply(TeamMembers, unlist)
+    TeamMembers <- lapply(TeamMembers, FUN = function(x){ data.frame(t(x), stringsAsFactors = F) })
+    TeamMembers <- do.call("bind_rows", TeamMembers)
+    # Mutate On key to join with Results
+    TeamMembers <- TeamMembers %>% mutate(key = 1:nrow(TeamMembers))
+    
+    # Now I need to Join back the Team Members to the Results
+    Full_Results <- full_join(Results, TeamMembers, by = "key") %>% 
+      select(-key, -TeamMemberList)
+  }
+  
+  # Write to CSV
+  # Wrapped in a unique because we only need 1 filename
+  # The team event stuff was being a little silly
+  output_file_name <- unique(paste0("Output Folder/302 Short Track CSVs/", 
+                                    # Only the actual date, not the time of game
+                                    Event, "-", substr(Date, 1,10), ".csv"))
+  
+  write.csv(x = Full_Results, 
+            file = output_file_name,
+            row.names = FALSE)
+  print(paste(Event, "was a Success", "/n"))
+  
+}
---
+++ Parsing Files/307 Skeleton Parsing.R
@@ -0,0 +1,58 @@
+# Luge Parsing
+
+# Bobsled Parsing
+library(tidyverse)
+library(jsonlite)
+library(lubridate)
+
+# Read in All File Names
+# Code Stolen From:
+# https://www.geeksforgeeks.org/read-all-files-in-directory-using-r/#:~:text=To%20list%20all%20files%20in,files%20in%20the%20specified%20directories.
+all_files <- list.files(path = "Output Folder/307 Skeleton JSONs",
+                        # To make sure I grab only the relevant files 
+                        pattern = "Match ID")
+
+
+# Comment Out For Loop For now
+for (json_file_name in all_files){
+  
+  # Sanity Check
+  print(json_file_name)
+  
+  # Read in the json file
+  # Don't know how this works, but it does.
+  # Stolen From Stack Overflow:
+  # https://stackoverflow.com/questions/38074926/unable-to-parse-locally-stored-json-file-with-special-character-like-backslash
+  file_path <- paste0("Output Folder/307 Skeleton JSONs/", json_file_name)
+  raw_json <- fromJSON(gsub("\\\\","",readLines(file_path)))
+  
+  # Date of Match and Gender
+  Date <- raw_json$Result$PhaseList$DateTimes$Start$c_Local
+  # Gender is in the json file name
+  Event <- str_split(json_file_name, 
+                     pattern = "Match ID", 
+                     n = 2)[[1]][1] %>% 
+    # Trim Whitespace
+    str_trim()
+  
+  # Get Basic Results
+  Results <- raw_json$Result$PhaseList$ParticipantList %>% 
+    as.data.frame()
+  
+  # Phase List and Team Member List needs to be unlisted
+  Results$PhaseResultList <- unlist(Results$PhaseResultList)
+  Results$TeamMemberList <- unlist(Results$TeamMemberList)
+  Full_Results <- Results
+  
+  # Write to CSV
+  # Wrapped in a unique because we only need 1 filename
+  # The team event stuff was being a little silly
+  output_file_name <- unique(paste0("Output Folder/307 Skeleton CSVs/", 
+                                    # Only the actual date, not the time of game
+                                    Event, "-", substr(Date, 1,10), ".csv"))
+  
+  write.csv(x = Full_Results, 
+            file = output_file_name,
+            row.names = FALSE)
+  print(paste(Event, "was a Success", "/n"))
+}
