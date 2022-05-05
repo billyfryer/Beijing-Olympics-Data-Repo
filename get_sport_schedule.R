fc8d000555027c99602abb7a57b01b4d294a8edf
Billy Fryer <billyfryer@att.net>
2022-05-05 01:37:20 -0400

    Fix duplicated Columns in Sport Schedules

--- get_sport_schedule.R
+++ get_sport_schedule.R
@@ -1,5 +1,5 @@
 # Get Schedule Sport
-library(magrittr)
+library(tidyverse)
 library(jsonlite)
 get_sport_schedule <- function(sportID){
   url <- paste0("https://api-gracenote.nbcolympics.com/svc/games_v2.svc/json/GetScheduleSport?competitionSetId=2&season=20212022&sportId=", sportID, "&languageCode=2")
@@ -13,6 +13,11 @@ get_sport_schedule <- function(sportID){
   if(sportID %in% c(113, 212)) {
     jackpot$Match.PeriodList <- unlist(jackpot$Match.PeriodList)
   }
+  
+  # Get Rid of Columns with duplicated names
+  # Code Stolen From:
+  # https://statisticsglobe.com/remove-columns-duplicate-names-from-data-frame-r#example-1-delete-columns-with-duplicate-names-using-duplicated-colnames-functions
+  jackpot <- jackpot[ , !duplicated(colnames(jackpot))]  
 
   return(jackpot)
 }
