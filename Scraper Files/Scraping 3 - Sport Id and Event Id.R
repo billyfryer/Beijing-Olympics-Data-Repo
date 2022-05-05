d175fe45bb30d602d783730e71bccaeea7055ad1
Billy Fryer <billyfryer@att.net>
2022-05-05 11:13:11 -0400

    Rescrape JSONs and rename folders

--- Scraper Files/Scraping 3 - Sport Id and Event Id.R
+++ Scraper Files/Scraping 3 - Sport Id and Event Id.R
@@ -7,114 +7,65 @@ source("get_event_phase.R")
 source("get_sport_data.R")
 # These are the rest of the functions that I need to pull from
 
-sport_list <- read_csv("Data/Sport List.csv")
+all_schedules <- paste0("Data/Sport Schedules/",
+                        list.files("Data/Sport Schedules"))
 
+lookup_df <- data.frame(SportID = c(),
+                        SportTypeID = c(),
+                        MatchID = c(),
+                        PhaseID = c())
 
-sportids <- sport_list %>% pull(n_SportID)
-
-lookup_df <- data.frame(sportID = c(),
-                        phaseID = c(),
-                        matchID = c())
-for (i in 1:length(sportids)) {
-  print(paste("Sport ID ", i, "/", length(sportids)))
-  id <- sportids[i]
+for (schedule in all_schedules) {
   
-  sport <- sport_list$c_Sport[i]
-
-  event_phase <- get_event_phase(id) 
+  print(paste("Testing:", schedule))
+  raw_csv <- read_csv(schedule)
   
-  phase_id <- event_phase %>% 
-    pull(n_EventPhaseID)
-  
-  SportTypeID <- event_phase %>% 
-    pull(n_SportTypeID) %>% 
-    unique()
-  
-  if(unique(SportTypeID) == 3) {
-    expanded <- expand.grid(sportID = id, 
-                            phaseID = phase_id, 
-                            matchID = NA)
+  # If it's Hockey or Curling
+  if(str_detect(schedule, pattern = "113") | str_detect(schedule, pattern = "212")) {
+    temp <- raw_csv %>% 
+      select(SportID = Sport.n_ID,
+             SportTypeID = Sport.n_TypeID,
+             MatchID = Match.n_ID) %>% 
+      mutate(PhaseID = NA) %>% 
+      select(SportID, SportTypeID, MatchID, PhaseID)
   } else {
-    expanded <- expand.grid(sportID = id, 
-                            phaseID = NA, 
-                            matchID = phase_id)
-  }
-  lookup_df <- bind_rows(lookup_df, expanded)
+    # Other Sports besides Hockey and Curling
+    temp <- raw_csv %>% 
+      select(SportID = Sport.n_ID,
+             SportTypeID = Sport.n_TypeID,
+             PhaseID = n_MatchID) %>% 
+      mutate(MatchID = NA) %>% 
+      select(SportID, SportTypeID, MatchID, PhaseID)
+    }
+  
+  lookup_df <- bind_rows(lookup_df, temp)
 }
 
 ##############################################
-### phase ID Sports
+### Grab Data For All Sports
 ##############################################
-phase_sports <- lookup_df %>% 
-  filter(is.na(matchID))
+for (i in 1:nrow(lookup_df)) {
+  
+  # Pull Individual Values Out of Each Row
+  SportID <- lookup_df[i,]$SportID
+  SportTypeID <- lookup_df[i,]$SportTypeID
+  MatchID <- lookup_df[i,]$MatchID
+  PhaseID <- lookup_df[i,]$PhaseID
+  
 
-phase_sports <- left_join(phase_sports, sport_list,
-                          by = c("sportID" = "n_SportID"))
-
-# Works for Match IDs sports
-for (sport_id in unique(phase_sports$sportID)){
-  
-  sport <- phase_sports %>% 
-    filter(sportID == sport_id) %>% 
-    pull(c_Sport) %>% 
-    unique()
-  
-  # New Schedule
-  phase_id_key <- get_event_phase(sport_id) %>% 
-    select(sport_id = n_SportID,
-           match_id = n_EventPhaseID,
-           event_name = c_GenderEvent)
-  
-  # Internal For Loop for individual games
-  for (i in 1:nrow(phase_id_key)) {
-    sport_id <- as.numeric(phase_id_key[i, 1])
-    phase_id <- as.numeric(phase_id_key[i,2])
-    event_name <- as.character(phase_id_key[i,3])
-    print(paste("Event",event_name, ":", i, "/", nrow(phase_id_key)))
-    sport_json <- get_sport_data(sportId = sport_id, phaseId = phase_id)
-    export_json <- toJSON(sport_json)
-    output_path <- paste0("Data/", sport_id, " JSONs/",
-                          phase_id, ".json")    
-    write(export_json, file = output_path)
+  if(SportTypeID == 1) {
+    print(paste("SportID:", SportID, "MatchID:", MatchID, i, "/", nrow(lookup_df)))
+    data <- get_sport_data(sportId = SportID, matchId = MatchID)
+    output_path <- paste0("Data/", SportID, " JSONs/", MatchID, ".json")
+  } else {
+    print(paste("SportID:", SportID, "PhaseID:", PhaseID, i, "/", nrow(lookup_df)))
+    data <- get_sport_data(sportId = SportID, phaseId = PhaseID)
+    output_path <- paste0("Data/", SportID, " JSONs/", PhaseID, ".json")
+    
   }
-}
-
-##############################################
-### match ID Sports
-##############################################
-match_sports <- lookup_df %>% 
-  filter(!is.na(matchID))
-
-match_sports <- left_join(match_sports, sport_list, by = c("sportID" = "n_SportID"))
-
-for (sport_id in unique(match_sports$sportID)){
   
-  sport <- match_sports %>% 
-    filter(sportID == sport_id) %>% 
-    pull(c_Sport) %>% 
-    unique()
+  # Save as JSON
+  export_json <- toJSON(data)
+  write(export_json, file = output_path)
   
-  # New Schedule
-  schedule <- get_sport_schedule(sport_id) %>% 
-    .$DateList.EventPhaseMatchList %>% 
-    bind_rows() %>%
-    select(Match, Sport, GenderEvent) %>% 
-    unpack(cols = c("Match", "Sport", "GenderEvent"),
-          names_repair = "unique") %>% 
-    select("Sport_ID" = n_ID...11, 
-           "Event_Name" = c_Name...16, 
-           "Match_ID" = n_ID...1)
-  
-  # Internal For Loop for individual games
-  for (i in 1:nrow(schedule)) {
-    print(paste("Match ", i, "/", nrow(schedule)))
-    sport_id = schedule[i, 1]
-    event_name = schedule[i,2]
-    match_id = schedule[i,3]
-    sport_json <- get_sport_data(sportId = sport_id, matchId = match_id)
-    export_json <- toJSON(sport_json)
-    output_path <- paste0("Data/", sport_id, " JSONs/",
-                          match_id, ".json")
-    write(export_json, file = output_path)
-  }
 }
