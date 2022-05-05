89d216100a26280e906b75b175c4a883f5280759
Billy Fryer <billyfryer@att.net>
2022-05-04 23:20:36 -0400

    Sport Schedules and Event Matches Files

--- Scraper Files/Scraping 2 - Only Sport Id.R
+++ Scraper Files/Scraping 2 - Only Sport Id.R
@@ -1,8 +1,9 @@
 library(tidyverse)
+library(qdapRegex)
 # Only things we need is SportIds
 sport_list <- read_csv("Data/Sport List.csv")
 
-sportids <- sport_list %>% pull(n_SportID)
+sportids <- sport_list %>% pull(SportID)
 
 ### Create Folders Based on SportID
 for (i in 1:length(sportids)) {
@@ -18,7 +19,19 @@ for (i in 1:length(sportids)) {
   id <- sportids[i]
   print(paste("Sport ID ", i, "/", length(sportids)))
   event_phase <- get_event_phase(id)
-  output_path <- paste0("Data/", id, "/Event Phase.csv")
+  output_path <- paste0("Data/Event Matches/", id, " Event Matches.csv")
+  # Change "Phase" to match
+  names(event_phase) <- str_replace_all(names(event_phase), 
+                                  pattern = "Phase",
+                                  replacement = "Match")
+  
+  # Clean Names a bit by getting rid of underscore prefix
+  names(event_phase) <- str_split(names(event_phase), 
+                                  pattern = "_", 
+                                  n = 2, 
+                                  simplify = TRUE)[,2]
+  # Final Name Cleaning
+  event_phase <- event_phase %>% janitor::clean_names(case = "big_camel")
   # write.csv(event_phase, output_path,
   #           row.names = FALSE)
 }
@@ -29,7 +42,25 @@ for (i in 1:length(sportids)) {
   id <- sportids[i]
   print(paste("Sport ID ", i, "/", length(sportids)))
   sport_schedule <- get_sport_schedule(id)
-  export_json <- toJSON(sport_schedule)
-  output_path <- paste0("Data/", id, "/Sport Schedule.json")
-  # write(export_json, file = output_path)
+  
+  # Change "Phase" to match
+  names(sport_schedule) <- str_replace_all(names(sport_schedule), 
+                                        pattern = "Phase",
+                                        replacement = "Match")
+  
+  # Clean Names a bit by getting rid of most of the underscore prefixes
+  names(sport_schedule) <- qdapRegex::rm_between(names(sport_schedule), 
+                                  left = ".",
+                                  right = "_")
+  # Final Name Cleaning
+  sport_schedule <- sport_schedule %>% 
+    janitor::clean_names(case = "big_camel") %>% 
+    data.frame()
+  
+  # Get Rid of Row Names
+  rownames(sport_schedule) <- NULL
+  
+  output_path <- paste0("Data/Sport Schedules/", id, " Sport Schedule.csv")
+  write.csv(sport_schedule, output_path,
+            row.names = FALSE)
 }
--- get_sport_schedule.R
+++ get_sport_schedule.R
@@ -1,11 +1,12 @@
 # Get Schedule Sport
 library(magrittr)
 library(jsonlite)
-get_sport_schedule <- function(sportId){
-  url <- paste0("https://api-gracenote.nbcolympics.com/svc/games_v2.svc/json/GetScheduleSport?competitionSetId=2&season=20212022&sportId=", sportId, "&languageCode=2")
+get_sport_schedule <- function(sportID){
+  url <- paste0("https://api-gracenote.nbcolympics.com/svc/games_v2.svc/json/GetScheduleSport?competitionSetId=2&season=20212022&sportId=", sportID, "&languageCode=2")
 
   jackpot <- jsonlite::fromJSON(url)  %>%  
-    as.data.frame()
+    as.data.frame() %>% 
+    unnest(col = DateList.EventPhaseMatchList)
   
   return(jackpot)
 }
