f7354c80e8250b8e4baf431b577ff3a7b585b588
Billy Fryer <billyfryer@att.net>
2022-05-05 22:54:47 -0400

    307 CSVs Good

--- Parsing Files/307 Skeleton Parsing.R
+++ Parsing Files/307 Skeleton Parsing.R
@@ -1,22 +1,20 @@
-# Luge Parsing
+# Figure Skating Parsing
 
-# Bobsled Parsing
+# Libraries
 library(tidyverse)
 library(jsonlite)
 library(lubridate)
 
-key <- data.frame(Event = c(),
-                  MatchID = c())
-
 # Read in All File Names
 # Code Stolen From:
 # https://www.geeksforgeeks.org/read-all-files-in-directory-using-r/#:~:text=To%20list%20all%20files%20in,files%20in%20the%20specified%20directories.
-all_files <- list.files(path = "Data/307 Skeleton JSONs")
+all_files <- list.files(path = "Data/217 JSONs")
+all_files <- paste0("Data/217 JSONs/", all_files)
 
 
-# Comment Out For Loop For now
-for (json_file_name in all_files){
+for (i in 1:length(all_files)){
   
+  json_file_name <- all_files[i]
   # Sanity Check
   print(json_file_name)
   
@@ -24,15 +22,16 @@ for (json_file_name in all_files){
   # Don't know how this works, but it does.
   # Stolen From Stack Overflow:
   # https://stackoverflow.com/questions/38074926/unable-to-parse-locally-stored-json-file-with-special-character-like-backslash
-  file_path <- paste0("Data/307 Skeleton JSONs/", json_file_name)
-  raw_json <- fromJSON(gsub("\\\\","",readLines(file_path)))
+  raw_json <- fromJSON(gsub("\\\\","",readLines(json_file_name)))
   
   # Date of Match and Gender
   Date <- raw_json$Result$PhaseList$DateTimes$Start$c_Local
   # Get MatchID
-  MatchID <- str_remove(json_file_name, pattern = ".json")
+  MatchID <- raw_json$PhaseInfo$n_PhaseID
   # Event
   Event <- raw_json$PhaseInfo$Event$c_Name
+  
+  Results <- raw_json$Result$PhaseList$ParticipantList
   
   # Do call if necessary, otherwise do as.data.frame
   if (length(Results) == 1) {
