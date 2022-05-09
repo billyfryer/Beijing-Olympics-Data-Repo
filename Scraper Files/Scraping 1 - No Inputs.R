8b23b857551c75b9be955046b4011d52205bfe8e
Billy Fryer <billyfryer@att.net>
2022-05-09 16:18:50 -0400

    Scraper File No Inputs Update

--- Scraper Files/Scraping 1 - No Inputs.R
+++ Scraper Files/Scraping 1 - No Inputs.R
@@ -3,17 +3,17 @@ library(tidyverse)
 ### Getting Date list
 source("get_date_list.R")
 date_list <- get_date_list()
-write.csv(date_list, "Data/Date List.csv",
+write.csv(date_list, "Data/Date_List.csv",
 row.names = FALSE)
 
 ### Getting Schedule Matrix
 source("get_schedule_matrix.R")
 schedule_matrix <- get_schedule_matrix()
-write.csv(schedule_matrix, "Data/Schedule Matrix.csv",
+write.csv(schedule_matrix, "Data/Schedule_Matrix.csv",
 row.names = FALSE)
 
 ### Getting Sport List
 source("get_sport_list.R")
 sport_list <- get_sport_list()
-write.csv(sport_list, "Data/Sport List.csv",
+write.csv(sport_list, "Data/Sport_List.csv",
 row.names = FALSE)
